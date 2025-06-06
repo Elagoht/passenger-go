5239385a46e5fa8898ea341d6a6e4a5c9fb630b9
Furkan Baytekin <furkanbaytekin@gmail.com>
2025-06-06 19:10:58 +0300

    refactor: improve error handling and update router initialization in AuthController

--- backend/controllers/auth.go
+++ backend/controllers/auth.go
@@ -5,12 +5,13 @@ import (
 	"net/http"
 	"passenger-go/backend/schemas"
 	"passenger-go/backend/services"
+	"passenger-go/backend/utilities"
 
 	"github.com/go-chi/chi"
 )
 
 type AuthController struct {
-	authRouter  *chi.Mux
+	authRouter  *utilities.Router
 	authService *services.AuthService
 }
 
@@ -21,96 +22,97 @@ func NewAuthController() *AuthController {
 }
 
 func (controller *AuthController) MountAuthRouter(router *chi.Mux) {
-	controller.authRouter = chi.NewRouter()
+	controller.authRouter = utilities.NewRouter(chi.NewRouter())
 	controller.authRouter.Get("/status", controller.Status)
 	controller.authRouter.Post("/register", controller.RegisterUser)
 	controller.authRouter.Post("/validate", controller.CompleteRegistration)
 	controller.authRouter.Post("/login", controller.LoginUser)
 	controller.authRouter.Post("/update", controller.UpdatePassphrase)
 
-	router.Mount("/auth", controller.authRouter)
+	router.Mount("/auth", controller.authRouter.Mux())
 }
 
 func (controller *AuthController) Status(
 	writer http.ResponseWriter,
 	request *http.Request,
-) {
+) (err error) {
 	initialized, err := controller.authService.Status()
 
 	if err != nil {
-		writer.WriteHeader(http.StatusInternalServerError)
-		return
+		return err
 	}
 
 	json.NewEncoder(writer).Encode(schemas.ResponseIsInitialized{
 		Initialized: initialized,
 	})
+
+	return nil
 }
 
 func (controller *AuthController) RegisterUser(
 	writer http.ResponseWriter,
 	request *http.Request,
-) {
+) (err error) {
 	var body schemas.RequestAuthRegister
 	json.NewDecoder(request.Body).Decode(&body)
 
 	recovery, err := controller.authService.RegisterUser(body.Passphrase)
 	if err != nil {
-		writer.WriteHeader(http.StatusInternalServerError)
-		json.NewEncoder(writer).Encode(schemas.ResponseError{
-			Code:    "INTERNAL_SERVER_ERROR",
-			Message: err.Error(),
-		})
-		return
+		return err
 	}
 
 	json.NewEncoder(writer).Encode(schemas.ResponseAuthRegister{
 		Recovery: recovery,
 	})
+
+	return nil
 }
 
 func (controller *AuthController) CompleteRegistration(
 	writer http.ResponseWriter,
 	request *http.Request,
-) {
+) (err error) {
 	var body schemas.RequestAuthCompleteRegistration
 	json.NewDecoder(request.Body).Decode(&body)
 
-	err := controller.authService.CompleteRegistration(body.Recovery)
+	err = controller.authService.CompleteRegistration(body.Recovery)
 	if err != nil {
-		writer.WriteHeader(http.StatusInternalServerError)
-		json.NewEncoder(writer).Encode(schemas.ResponseError{
-			Code:    "INTERNAL_SERVER_ERROR",
-			Message: err.Error(),
-		})
-		return
+		return err
 	}
 
-	writer.WriteHeader(http.StatusOK)
+	return nil
 }
 
 func (controller *AuthController) LoginUser(
 	writer http.ResponseWriter,
 	request *http.Request,
-) {
+) (err error) {
 	var body schemas.RequestAuthLogin
 	json.NewDecoder(request.Body).Decode(&body)
 
 	token, err := controller.authService.LoginUser(body.Passphrase)
 	if err != nil {
-		writer.WriteHeader(http.StatusInternalServerError)
-		return
+		return err
 	}
 
 	json.NewEncoder(writer).Encode(schemas.ResponseAuthLogin{
 		Token: token,
 	})
+
+	return nil
 }
 
 func (controller *AuthController) UpdatePassphrase(
 	writer http.ResponseWriter,
 	request *http.Request,
-) {
+) (err error) {
 	var body schemas.RequestAuthUpdatePassphrase
 	json.NewDecoder(request.Body).Decode(&body)
+
+	err = controller.authService.UpdatePassphrase(body.Passphrase)
+	if err != nil {
+		return err
+	}
+
+	return nil
 }
