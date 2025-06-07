f409e0adf12a6bc602401d9166a70890db67dbcb
Furkan Baytekin <furkanbaytekin@gmail.com>
2025-06-07 21:08:40 +0300

    fix: update pagination parameters to handle default values for page and take

--- backend/utilities/pagination.go
+++ backend/utilities/pagination.go
@@ -13,18 +13,28 @@ type Pagination struct {
 }
 
 func PaginationParams(query *http.Request) (*Pagination, *schemas.APIError) {
-	page, err := strconv.Atoi(query.URL.Query().Get("page"))
+	page := query.URL.Query().Get("page")
+	if page == "" {
+		page = "1"
+	}
+
+	pageInt, err := strconv.Atoi(page)
 	if err != nil {
 		return nil, schemas.NewAPIError(schemas.ErrInvalidRequest, "Invalid page", err)
 	}
 
-	take, err := strconv.Atoi(query.URL.Query().Get("take"))
+	take := query.URL.Query().Get("take")
+	if take == "" {
+		take = "10"
+	}
+
+	takeInt, err := strconv.Atoi(take)
 	if err != nil {
 		return nil, schemas.NewAPIError(schemas.ErrInvalidRequest, "Invalid take", err)
 	}
 
 	return &Pagination{
-		Page: page,
-		Take: take,
+		Page: pageInt,
+		Take: takeInt,
 	}, nil
 }
