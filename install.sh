9a8e45b4d1837530ad8e2a3c22a5ab2ca2f6b360
Furkan Baytekin <furkanbaytekin@gmail.com>
2025-06-17 22:30:06 +0300

    feat: improve installation script with enhanced output formatting and error handling

--- install.sh
+++ install.sh
@@ -1,42 +1,54 @@
 #!/bin/bash
 
-echo "Installing Passenger Go..."
+infoPrint() {
+    echo -e "\033[1;32m$1\033[0m"
+}
 
-echo "Checking architecture..."
+errorPrint() {
+    echo -e "\033[1;31m$1\033[0m"
+}
+
+successPrint() {
+    echo -e "\033[1;32m$1\033[0m"
+}
+
+infoPrint "Installing Passenger Go..."
+
+infoPrint "Checking architecture..."
 arch=$(uname -m)
 if [ "$arch" == "x86_64" ]; then
     arch="amd64"
 elif [ "$arch" == "aarch64" ]; then
     arch="arm64"
 else
-    echo "Unsupported architecture: $arch"
+    errorPrint "Unsupported architecture: $arch"
     exit 1
 fi
 
-echo "Architecture: $arch"
+infoPrint "Architecture: $arch"
 
 # If not linux, exit
 if [ "$(uname -s)" != "Linux" ]; then
-    echo "This script is only for Linux"
+    errorPrint "This script is only for Linux"
     exit 1
 fi
 
-echo "Creating directory..."
+infoPrint "Creating directory..."
 sudo mkdir -p /opt/passenger-go
 
-echo "Downloading latest release..."
+infoPrint "Downloading latest release..."
 sudo curl -sL "https://github.com/Elagoht/passenger-go/releases/latest/download/passenger-go-linux-$arch" -o /opt/passenger-go/passenger-go
 
-echo "Setting permissions..."
+infoPrint "Setting permissions..."
 sudo chmod +x /opt/passenger-go/passenger-go
 sudo chown -R $(whoami) /opt/passenger-go
-echo "Permissions set"
+successPrint "Permissions set"
 
-echo "Copying service file..."
+infoPrint "Copying service file..."
 sudo cp passenger-go.service /etc/systemd/system/passenger-go.service
 sudo sed -i "s/{USER}/$(whoami)/g" /etc/systemd/system/passenger-go.service
 
-echo "If you will set a custom PORT, write it here."
+infoPrint "If you will set a custom PORT, write it here."
 read -p "PORT: " port
 if [ -z "$port" ]; then
     port="8080"
@@ -45,12 +57,12 @@ fi
 if [ -n "$port" ]; then
     sudo sed -i "s/{PORT}/$port/g" /etc/systemd/system/passenger-go.service
     echo "PORT=$port" >> /opt/passenger-go/.env
-    echo "PORT set to $port"
+    successPrint "PORT set to $port"
 else
-    echo "Default PORT will be used"
+    infoPrint "Default PORT will be used"
 fi
 
-echo Now we need this ENV variables:
+infoPrint "Now we need this ENV variables:"
 read -p "JWT_SECRET: " jwt_secret
 echo "JWT_SECRET=$jwt_secret" >> /opt/passenger-go/.env
 read -p "AES_GCM_SECRET: " aes_gcm_secret
@@ -58,10 +70,10 @@ echo "AES_GCM_SECRET=$aes_gcm_secret" >> /opt/passenger-go/.env
 read -p "SALT: " salt
 echo "SALT=$salt" >> /opt/passenger-go/.env
 
-echo "Reloading systemd..."
+infoPrint "Reloading systemd..."
 sudo systemctl daemon-reload
 
-echo "Enabling and starting service..."
+infoPrint "Enabling and starting service..."
 sudo systemctl enable --now passenger-go.service
 
-echo "Done!"
+successPrint "Done!"
